e57177cf0685eca3e88827fa1fd9013fcfac4529
igorzakhar <igorzakhar@gmail.com>
2017-06-01 19:00:32 +0300

    To rewrite all code

--- duplicates.py
+++ duplicates.py
@@ -1,35 +1,58 @@
 import os
 import sys
+import hashlib
 from collections import defaultdict, namedtuple
 
 
-File_object = namedtuple('File', 'name size')
-
-def walk(directory):
-
-    dict_of_path = defaultdict(list)
-
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            full_path = os.path.join(dirpath, filename)
-            file_size = os.path.getsize(full_path)
-            current_file = File_object(filename, file_size)
-            dict_of_path[current_file].append(full_path)
-    return dict_of_path
+File_object = namedtuple('File', 'name size checksum')
 
 
-def print_duplicates(dict_of_path):
-    for current_file in dict_of_path:
-        if len(dict_of_path[current_file]) > 1:
-            print('Filename:', current_file.name, '/ size', current_file.size, 'bytes')
-            print('-' * 50)
-            for path in dict_of_path[current_file]:
-                print(path)
-            print('-' * 50,'\n')
+def md5_checksum(filename, blocksize=65536):
+    checksum = hashlib.md5()
+    with open(filename, 'rb') as fp:
+        buffer = fp.read(blocksize)
+        while len(buffer) > 0:
+            checksum.update(buffer)
+            buffer = fp.read(blocksize)
+    return checksum.hexdigest()
+
+
+def enumerate_path(path = '/tmp/duplicates'):
+    path_collection = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for f in filenames:
+            fulpath = os.path.join(dirpath, f)
+            path_collection.append(fulpath)
+    return path_collection
+
+
+def get_duplicates(path_collection):
+    records  = defaultdict(list)
+    for file in path_collection:
+        filename = os.path.basename(file)
+        file_size = os.path.getsize(file)
+        checksum = md5_checksum(file)
+        compaund_key = File_object(filename, file_size, checksum)
+        records[compaund_key].append(file)
+    duplicates = {k: v for k, v in records.items() if len(v) > 1}
+    return duplicates
+  
+
+def output_results(dict_of_duplicates):
+    for file in dict_of_duplicates:
+        print('Filename:', file.name, '/ size', file.size, 'bytes', '/ checksum', file.checksum)
+        print('-' * 50)
+        for path in dict_of_duplicates[file]:
+            print(path)
+        print('-' * 50,'\n')
 
 
 if __name__ == '__main__':
+    path_collection = enumerate_path()
+    dupes = get_duplicates(path_collection)
+    output_results(dupes)
     
+"""
     if len(sys.argv) > 1 and os.path.isdir(sys.argv[1]):
         filepath = sys.argv[1]
         dict_of_path = walk(filepath)
@@ -37,3 +60,4 @@ if __name__ == '__main__':
     else:
         print("Path not found.")
         print("Example launch: python duplicates.py <path>")
+    """
